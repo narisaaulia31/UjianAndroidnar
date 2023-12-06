7125334f873f87149bea34756f9750c0c393f67d
narisaaul31 <narisaaul31@gmail.com>
2023-12-06 20:39:58 +0700

    membuat perulangan 1 - 20 menskip bilangan genap menambahkan angka index kedalam item list

--- app/src/main/java/com/example/ujianandroid/ListActivity.java
+++ app/src/main/java/com/example/ujianandroid/ListActivity.java
@@ -9,7 +9,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
-
+import java.util.Collections;
 public class ListActivity extends AppCompatActivity {
 
     @Override
@@ -17,17 +17,19 @@ public class ListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
-        ListView lvNama = (ListView) findViewById(R.id.lvNama);
+        ListView lvNama = findViewById(R.id.lvNama);
 
-        ArrayList<String> daftar_nama = getIntent().getExtras().getStringArrayList("daftar_nama");
+        // Mendapatkan daftar nama dari Intent
+        ArrayList<String> daftar_nama = getIntent().getStringArrayListExtra("daftar_nama");
 
-        if(daftar_nama.isEmpty()){
-            daftar_nama.add("data masih kosong");
+        // Menyiapkan daftar angka ganjil dan nama yang sama
+        ArrayList<String> combinedList = new ArrayList<>();
+        for (int i = 1; i <= 20; i += 2) {
+            combinedList.add(i + ". " + (daftar_nama.isEmpty() ? "Nama Kosong" : daftar_nama.get(0)));
         }
 
-        ArrayAdapter<String> ad_nama = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, daftar_nama);
+        ArrayAdapter<String> ad_nama = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, combinedList);
 
         lvNama.setAdapter(ad_nama);
-
-    }
+}
 }
